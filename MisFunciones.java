c33b31570fa41aeb9c58a04dfd0c409e2258cf48
Male-D1ng <102553397+Male-D1ng@users.noreply.github.com>
2025-03-21 18:58:40 -0300

    Add files via upload

---
+++ MisFunciones.java
@@ -0,0 +1,121 @@
+package aed;
+
+class Funciones {
+    int cuadrado(int x) {
+        int res = x*x;
+        return res;
+    }
+
+    double distancia(double x, double y) {
+        double res = Math.sqrt(x*x + y*y);
+        return res;
+    }
+    boolean divideA(int n, int d) {
+        boolean res = ((n%d) == 0); 
+        return res; 
+    }
+    boolean esPar(int n) {
+        boolean res = divideA (n,2);
+        return res;
+    }
+
+    boolean esBisiesto(int n) {
+        boolean res = (divideA(n,4) && !(divideA(n,100)) || divideA(n,400));
+        return res;
+    }
+
+    int factorialIterativo(int n) {
+        // COMPLETAR
+        int res = 1;
+        while (n > 0) {
+           // int variable = (n-1);
+            res = res*n;
+            n = n-1;
+        }
+        return res;
+        }
+       /* if n == 0 {
+            res = res*1;
+        }
+        else n>0 {
+            res = res*n;
+            n-=1;
+        }
+        return res */ 
+
+    int factorialRecursivo(int n) {
+        // COMPLETAR
+        int res = 1;
+        if (n == 0) {
+            res = res*1;
+        }
+        else {
+            res = n*factorialRecursivo(n-1);
+        }
+        return res;
+    }
+
+    int primo (int n) {
+        int res = 0;
+        if (n==0 || n==1) {
+            res = 1;
+        }
+        int divisor = n;
+        while (divisor>=1 && n>1) { 
+            if (divideA(n, divisor)== true) {
+                res += 1;
+                divisor-=1;
+            }
+            else {
+                divisor-=1;
+            }
+        }
+        return res;
+
+    }
+
+    boolean esPrimo(int n) {
+        // COMPLETAR
+        boolean res;
+        if (primo (n) == 2){
+            res = true;
+        }
+        else {
+            res = false;
+        }
+        return res;
+    }
+
+ 
+
+
+    int sumatoria(int[] numeros) {
+        // COMPLETAR
+        return 0;
+    }
+
+    int busqueda(int[] numeros, int buscado) {
+        // COMPLETAR
+        return 0;
+    }
+
+    boolean tienePrimo(int[] numeros) {
+        // COMPLETAR
+        return false;
+    }
+
+    boolean todosPares(int[] numeros) {
+        // COMPLETAR
+        return false;
+    }
+
+    boolean esPrefijo(String s1, String s2) {
+        // COMPLETAR
+        return false;
+    }
+
+    boolean esSufijo(String s1, String s2) {
+        // COMPLETAR
+        return false;
+    }
+}
