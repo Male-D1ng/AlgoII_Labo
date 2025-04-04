77b19566f5d8edca86a07362f97c15b4d30eaf8b
Male-D1ng <102553397+Male-D1ng@users.noreply.github.com>
2025-04-04 17:49:04 -0300

    agenda terminado

--- Agenda.java
+++ Agenda.java
@@ -1,28 +1,41 @@
 package aed;
 
 public class Agenda {
+    private Fecha fecha;
+    private ArregloRedimensionableDeRecordatorios recordatorios;
+
 
     public Agenda(Fecha fechaActual) {
+        fecha = new Fecha(fechaActual);
+        recordatorios = new ArregloRedimensionableDeRecordatorios();
         // Implementar
+
     }
 
     public void agregarRecordatorio(Recordatorio recordatorio) {
         // Implementar
+        recordatorios.agregarAtras(recordatorio);
     }
 
     @Override
     public String toString() {
         // Implementar
-        return "";
+        String mensaje = fechaActual() + "\n" + ("=====\n"); 
+        for (int i = 0; i<recordatorios.longitud();i++ ){
+            if (recordatorios.obtener(i).fecha().equals(fechaActual())) {
+            mensaje += recordatorios.obtener(i) + "\n";
+                    }}
+        return mensaje;
     }
 
     public void incrementarDia() {
         // Implementar
+        fecha.incrementarDia();
     }
 
     public Fecha fechaActual() {
         // Implementar
-        return null;
+        return fecha;
     }
 
 }
