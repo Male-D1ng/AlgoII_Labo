3796596f4cb16eacc4e0fee205567fba49d37e5d
Male-D1ng <102553397+Male-D1ng@users.noreply.github.com>
2025-05-16 15:42:13 -0300

    Add files via upload

--- Iterador.java
+++ Iterador.java
@@ -7,23 +7,11 @@ interface Iterador<T> {
      * 
      */
     public boolean haySiguiente();
-    
-    /**
-     * Devuelve true si hay un elemento anterior en la colección.
-     * 
-     */
-    public boolean hayAnterior();
 
     /**
      * Devuelve el elemento siguiente en la colección y avanza el iterador.
      * 
      */
     public T siguiente();
-
-    /**
-     * Devuelve el elemento anterior en la colección y retrocede el iterador.
-     * 
-     */
-    public T anterior();
 
 }
